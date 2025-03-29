cdc7308eb967fc538c63cf79e5158673ecea4a72
vural4646 <58981997+vural4646@users.noreply.github.com>
2025-03-29 10:57:47 +0300

    Update vector_trigger.py

--- vector_trigger.py
+++ vector_trigger.py
@@ -2,9 +2,18 @@ import sounddevice as sd
 import vosk
 import json
 import os
+import numpy as np
 
 model = vosk.Model("model/vosk-model-small-tr-0.3")
 recognizer = vosk.KaldiRecognizer(model, 16000)
+
+def callback(indata, frames, time, status):
+    audio_data = np.frombuffer(indata, dtype=np.int16)
+    if recognizer.AcceptWaveform(audio_data.tobytes()):
+        result = recognizer.Result()
+        if "vector" in result.lower():
+            print("Vector tetiklendi!")
+            os.system("python3 pretrained_bot.py")
 
 def listen_trigger():
     with sd.RawInputStream(samplerate=16000, blocksize=8000, dtype='int16',
@@ -12,13 +21,5 @@ def listen_trigger():
         print("Vector tetikleyici dinliyor...")
         while True:
             sd.sleep(1000)
-
-def callback(indata, frames, time, status):
-    if recognizer.AcceptWaveform(indata.tobytes()):
-        result = recognizer.Result()
-        if "vector" in result.lower():
-            print("Vector tetiklendi!")
-            os.system("python3 pretrained_bot.py")
-
 
 listen_trigger()
