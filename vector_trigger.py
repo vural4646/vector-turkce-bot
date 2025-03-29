b93421cdc40758a97050eae6c19bd99ba93f1339
vural4646 <58981997+vural4646@users.noreply.github.com>
2025-03-29 09:49:05 +0300

    Create vector_trigger.py

---
+++ vector_trigger.py
@@ -0,0 +1,23 @@
+import sounddevice as sd
+import vosk
+import json
+import os
+
+model = vosk.Model("model/vosk-model-small-tr-0.3")
+recognizer = vosk.KaldiRecognizer(model, 16000)
+
+def listen_trigger():
+    with sd.RawInputStream(samplerate=16000, blocksize=8000, dtype='int16',
+                           channels=1, callback=callback):
+        print("Vector tetikleyici dinliyor...")
+        while True:
+            sd.sleep(1000)
+
+def callback(indata, frames, time, status):
+    if recognizer.AcceptWaveform(indata):
+        result = json.loads(recognizer.Result())
+        if 'vector' in result['text']:
+            print("Tetikleyici kelime duyuldu.")
+            os.system("python3 pretrained_bot.py")
+
+listen_trigger()
